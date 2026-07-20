824565bb309bab1b7e22eab75fcfe5e26e5c0f3c
arowojolu17-spec <arowojolu17@gmail.com>
2026-07-20 21:02:54 +0100

    Update page.tsx

--- app/upgrade/payment/page.tsx
+++ app/upgrade/payment/page.tsx
@@ -32,7 +32,7 @@ export default function UpgradePaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("6801696650")
+    navigator.clipboard.writeText("0011112774")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -95,7 +95,7 @@ export default function UpgradePaymentPage() {
                 <span>🔢</span> Account Number
               </p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">6801696650</p>
+                <p className="font-bold">0011112774</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -109,14 +109,14 @@ export default function UpgradePaymentPage() {
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🏦</span> Bank Name
               </p>
-              <p className="font-bold">MONIEPOINT MFB</p>
+              <p className="font-bold">DASH MFB</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🚹</span> Account Name
               </p>
-              <p className="font-bold">Paygo-AKINMUSAYO AKINNAGBE AROWOJOLU</p>
+              <p className="font-bold">Paygo-TIMILEHIN AROWOJOLU</p>
             </div>
           </div>
 
