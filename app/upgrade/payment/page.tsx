0eac5df0ab1975d919137e25b981c162027f2c0b
arowojolu17-spec <arowojolu17@gmail.com>
2026-09-01 22:33:38 +0100

    Update account name in payment page

--- app/upgrade/payment/page.tsx
+++ app/upgrade/payment/page.tsx
@@ -116,7 +116,7 @@ export default function UpgradePaymentPage() {
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🚹</span> Account Name
               </p>
-              <p className="font-bold">Paygo-FRIDAY AROWOJOLU</p>
+              <p className="font-bold">Paygo-FRIDAY OLUWOLE</p>
             </div>
           </div>
 
