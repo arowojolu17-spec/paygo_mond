b76294f378f4ccd0682ae96696f2288238b4e02d
arowojolu17-spec <arowojolu17@gmail.com>
2026-09-01 22:29:59 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -30,7 +30,7 @@ export default function PaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("1100078990")
+    navigator.clipboard.writeText("3002292542")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -89,7 +89,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">1100078990</p>
+                <p className="font-bold">3002292542</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -101,12 +101,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">KOLOMONI MFB</p>
+              <p className="font-bold">MONIEPOINT MFB</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">Ikujuni Monday Tila</p>
+              <p className="font-bold">FRIDAY OLUWOLE</p>
             </div>
           </div>
 
